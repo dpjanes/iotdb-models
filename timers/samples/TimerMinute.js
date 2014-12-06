a2eb87ec7c88f666407104ba6a5a3b3efc9920a9
David Janes <davidjanes@davidjanes.com>
2014-12-06 07:49:33 -0500

    timers are no longer models (bad idea)

--- timers/samples/TimerMinute.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-var iotdb = require('iotdb')
-var iot = iotdb.iot()
-
-var clock = iot.connect("timer-minute")
-clock.on('when', function(thing, attribute, value) {
-    console.log("+ change", thing.state())
-})
